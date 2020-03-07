f55294ee37295d6aebe02617e19848512ebad08a
Christoph Gutmann <christoph.gutmann@gmx.ch>
2020-03-07 12:43:11 +0100

    read a nqfanin-file with own code using default ecore editor

--- com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.fanin.nq/src/com/zazuko/rdfmapping/fanin/nq/nqfanin/util/RealNqFaninResourceFactoryImpl.java
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.fanin.nq/src/com/zazuko/rdfmapping/fanin/nq/nqfanin/util/RealNqFaninResourceFactoryImpl.java
@@ -1,8 +1,13 @@
 package com.zazuko.rdfmapping.fanin.nq.nqfanin.util;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+
 public class RealNqFaninResourceFactoryImpl extends NqFaninResourceFactoryImpl{
 
-	public RealNqFaninResourceFactoryImpl() {
-		System.out.println(this.getClass());
+	@Override
+	public Resource createResource(URI uri) {
+		return new RealNqFaninResourceImpl(uri);
 	}
+
 }
---
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.fanin.nq/src/com/zazuko/rdfmapping/fanin/nq/nqfanin/util/RealNqFaninResourceImpl.java
@@ -0,0 +1,36 @@
+package com.zazuko.rdfmapping.fanin.nq.nqfanin.util;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+import org.eclipse.emf.common.util.URI;
+
+import com.zazuko.rdfmapping.fanin.nq.nqfanin.NqFaninFactory;
+import com.zazuko.rdfmapping.fanin.nq.nqfanin.NqThing;
+
+public class RealNqFaninResourceImpl extends NqFaninResourceImpl {
+
+	public RealNqFaninResourceImpl(URI uri) {
+		super(uri);
+	}
+
+	@Override
+	protected void doLoad(InputStream inputStream, Map<?, ?> options) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+		reader.lines().forEach(line -> {
+			if (line == null || line.isEmpty()) {
+				return;
+			}
+			NqThing thing = NqFaninFactory.eINSTANCE.createNqThing();
+			this.getContents().add(thing);
+			thing.setName(line);
+		});
+	}
+	
+	
+
+}
