c2e26b931908bae7905c445e16d4405a870c940a
Christoph Gutmann <christoph.gutmann@gmx.ch>
2020-03-07 12:14:00 +0100

    plug in own resourcefactory

---
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.fanin.nq/src/com/zazuko/rdfmapping/fanin/nq/nqfanin/util/RealNqFaninResourceFactoryImpl.java
@@ -0,0 +1,8 @@
+package com.zazuko.rdfmapping.fanin.nq.nqfanin.util;
+
+public class RealNqFaninResourceFactoryImpl extends NqFaninResourceFactoryImpl{
+
+	public RealNqFaninResourceFactoryImpl() {
+		System.out.println(this.getClass());
+	}
+}
