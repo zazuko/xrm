2f52ad05dc6433f4def9ee91e12bf86bddab5a8e
Christoph Gutmann <christoph.gutmann@gmx.ch>
2020-03-10 22:03:16 +0100

    mvn-build nq

---
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.fanin.nq/src/com/zazuko/rdfmapping/fanin/nq/nqfanin/components/StringReplacingComponent.java
@@ -0,0 +1,63 @@
+package com.zazuko.rdfmapping.fanin.nq.nqfanin.components;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowComponent;
+import org.eclipse.emf.mwe2.runtime.workflow.IWorkflowContext;
+
+public class StringReplacingComponent implements IWorkflowComponent {
+
+	private String fileName;
+	private String oldValue;
+	private String newValue;
+
+	public String getOldValue() {
+		return oldValue;
+	}
+
+	public void setOldValue(String oldValue) {
+		this.oldValue = oldValue;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getNewValue() {
+		return newValue;
+	}
+
+	public void setNewValue(String newValue) {
+		this.newValue = newValue;
+	}
+
+	@Override
+	public void preInvoke() {
+
+	}
+
+	@Override
+	public void invoke(IWorkflowContext ctx) {
+		try {
+			Path path = Paths.get(fileName);
+			String pluginXml = new String(Files.readAllBytes(path));
+			pluginXml = pluginXml.replace(this.oldValue, this.newValue);
+			Files.write(path, pluginXml.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void postInvoke() {
+
+	}
+
+}
