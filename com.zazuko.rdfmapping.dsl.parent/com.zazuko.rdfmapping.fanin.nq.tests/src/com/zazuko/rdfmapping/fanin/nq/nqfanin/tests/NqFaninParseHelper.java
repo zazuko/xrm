95c78cb010606d5bed4f23e061a21af614c189d7
Christoph Gutmann <christoph.gutmann@gmx.ch>
2020-03-13 10:58:27 +0100

    read a folder for tests

--- com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.fanin.nq.tests/src/com/zazuko/rdfmapping/fanin/nq/nqfanin/tests/NqFaninParseHelper.java
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.fanin.nq.tests/src/com/zazuko/rdfmapping/fanin/nq/nqfanin/tests/NqFaninParseHelper.java
@@ -1,15 +1,20 @@
 package com.zazuko.rdfmapping.fanin.nq.nqfanin.tests;
 
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.eclipse.xtext.util.LazyStringInputStream;
 
+import com.zazuko.rdfmapping.fanin.nq.nqfanin.NqFaninRuntimeModule;
 import com.zazuko.rdfmapping.fanin.nq.nqfanin.util.RealNqFaninResourceImpl;
 
 public class NqFaninParseHelper {
@@ -17,23 +22,39 @@ public class NqFaninParseHelper {
 	public Resource parse(CharSequence text) throws Exception {
 		XtextResourceSet set = new XtextResourceSet();
 		URI uri = URI.createURI("__nqfanintestsfaked.nq");
-		RealNqFaninResourceImpl result = new RealNqFaninResourceImpl(uri );
+		RealNqFaninResourceImpl result = new RealNqFaninResourceImpl(uri);
 		set.getResources().add(result);
-		
+
 		InputStream in = new LazyStringInputStream(text == null ? "" : text.toString());
 		result.load(in, null);
 		return result;
 	}
-	
+
 	public <T extends EObject> T extractOne(Resource resource, Class<T> expectedType) {
 		@SuppressWarnings("unchecked")
-		List<T> candidates = resource.getContents().stream()
-				.filter(eo -> expectedType.isInstance(eo))
-				.map(eo -> (T)eo)
+		List<T> candidates = resource.getContents().stream().filter(eo -> expectedType.isInstance(eo)).map(eo -> (T) eo)
 				.collect(Collectors.toList());
 		if (candidates.size() != 1) {
 			throw new RuntimeException("expected exactly one element, but found " + candidates.size());
 		}
 		return candidates.get(0);
 	}
+
+	public ResourceSet parseFolder(String folder) throws Exception {
+		XtextResourceSet result = new XtextResourceSet();
+		Path dir = Paths.get(folder);
+		List<Path> nqFiles = Files.list(dir).peek(f -> System.out.println(f.getFileName())).filter(f -> f.getFileName().toString().endsWith("." + NqFaninRuntimeModule.NQ_FANIN_FILE_EXTENSION)).collect(Collectors.toList());
+		
+		for (Path nqFile : nqFiles) {
+			String absolute = nqFile.toAbsolutePath().toString();
+			URI uri = URI.createFileURI(absolute);
+
+			RealNqFaninResourceImpl resource = new RealNqFaninResourceImpl(uri);
+			result.getResources().add(resource);
+
+			resource.load(null);
+		}
+		return result;
+	}
+
 }
--- com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.fanin.nq.tests/src/com/zazuko/rdfmapping/fanin/nq/nqfanin/tests/NqFaninParsingTest.xtend
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.fanin.nq.tests/src/com/zazuko/rdfmapping/fanin/nq/nqfanin/tests/NqFaninParsingTest.xtend
@@ -3,6 +3,7 @@ package com.zazuko.rdfmapping.fanin.nq.nqfanin.tests
 import com.zazuko.rdfmapping.fanin.nq.nqfanin.NqThing
 import java.util.List
 import org.eclipse.emf.ecore.resource.Resource
+import org.eclipse.emf.ecore.resource.ResourceSet
 import org.junit.Assert
 import org.junit.jupiter.api.Test
 
@@ -33,5 +34,12 @@ class NqFaninParsingTest {
 		var int i = 0;
 		Assert.assertEquals("Foo", things.get(i++).name);
 		Assert.assertEquals("Bar", things.get(i++).name);
+	}
+	
+	@Test
+	def void readExampleSkos() {
+		val ResourceSet set = parseFolder("./data/skos");
+		Assert.assertEquals(2, set.resources.size);
+		
 	}
 }
