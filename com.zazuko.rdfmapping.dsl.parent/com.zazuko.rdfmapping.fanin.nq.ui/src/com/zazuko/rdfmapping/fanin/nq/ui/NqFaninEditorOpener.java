7bea127c817371868c8e543a7c3a215d4e6421a3
Christoph Gutmann <christoph.gutmann@gmx.ch>
2020-03-21 08:20:00 +0100

    hk

--- com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.fanin.nq.ui/src/com/zazuko/rdfmapping/fanin/nq/ui/NqFaninEditorOpener.java
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.fanin.nq.ui/src/com/zazuko/rdfmapping/fanin/nq/ui/NqFaninEditorOpener.java
@@ -19,37 +19,44 @@ import com.zazuko.rdfmapping.fanin.nq.nqfanin.domain.PositionInformation;
 public class NqFaninEditorOpener extends LanguageSpecificURIEditorOpener {
 
 	private static final Logger logger = Logger.getLogger(NqFaninEditorOpener.class);
-	
+
 	@Inject
 	private IResourceDescriptions rd;
 
 	@Override
 	protected void selectAndReveal(IEditorPart openEditor, URI uri, EReference crossReference, int indexInList,
 			boolean select) {
-		if (uri.fragment() != null) {
-			TextEditor editor = openEditor.getAdapter(TextEditor.class);
-			if (editor != null) {
-				try {
-					IResourceDescription resourceDescription = this.rd.getResourceDescription(uri.trimFragment());
-					
-					String expectedLocalObjectAddress = uri.fragment();
-					for (IEObjectDescription desc : resourceDescription.getExportedObjects()) {
-						Optional<PositionInformation> posO = PositionAdapter.fromIEObjectDescription(expectedLocalObjectAddress,
-								desc);
-						if (posO.isPresent()) {
-							PositionInformation position = posO.get();
-							editor.selectAndReveal(position.getStart(), position.getEnd() - position.getStart());
-							return;
-						}
-
-					}
-
-				} catch (Exception e) {
-					logger.warn("failed to find location", e);
-				}
-				editor.selectAndReveal(0, 0);
-			}
+		TextEditor editor = openEditor.getAdapter(TextEditor.class);
+		if (editor == null) {
+			return;
 		}
+
+		this.resolvePosition(uri).ifPresentOrElse(
+				position -> editor.selectAndReveal(position.getStart(), position.getEnd() - position.getStart()),
+				() -> editor.selectAndReveal(0, 0));
+	}
+
+	private Optional<PositionInformation> resolvePosition(URI uri) {
+		if (uri.fragment() == null) {
+			return Optional.empty();
+		}
+		try {
+			IResourceDescription resourceDescription = this.rd.getResourceDescription(uri.trimFragment());
+
+			String expectedLocalObjectAddress = uri.fragment();
+			for (IEObjectDescription desc : resourceDescription.getExportedObjects()) {
+				Optional<PositionInformation> posO = PositionAdapter.fromIEObjectDescription(expectedLocalObjectAddress,
+						desc);
+
+				if (posO.isPresent()) {
+					return posO;
+				}
+			}
+
+		} catch (Exception e) {
+			logger.warn("failed to find location for " + uri.toString(), e);
+		}
+		return Optional.empty();
 	}
 
 	@Override
