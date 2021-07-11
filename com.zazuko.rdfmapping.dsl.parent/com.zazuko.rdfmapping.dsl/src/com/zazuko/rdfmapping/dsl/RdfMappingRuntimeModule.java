1eb0817ddecb80786e48dad683fc62f9898400b2
Christoph Gutmann <christoph.gutmann@gmx.ch>
2021-07-11 15:51:49 +0200

    cleanup after rebuilding language infrastructure

--- com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.dsl/src/com/zazuko/rdfmapping/dsl/RdfMappingRuntimeModule.java
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.dsl/src/com/zazuko/rdfmapping/dsl/RdfMappingRuntimeModule.java
@@ -3,10 +3,12 @@ package com.zazuko.rdfmapping.dsl;
 import javax.inject.Named;
 
 import org.eclipse.xtext.conversion.IValueConverterService;
+import org.eclipse.xtext.formatting2.IFormatter2;
 import org.eclipse.xtext.resource.IDefaultResourceDescriptionStrategy;
 
 import com.google.inject.Injector;
 import com.google.inject.Provides;
+import com.zazuko.rdfmapping.dsl.formatting2.RealRdfMappingFormatter;
 import com.zazuko.rdfmapping.dsl.generator.common.GeneratorConstants;
 import com.zazuko.rdfmapping.dsl.generator.rml.CarmlDialect;
 import com.zazuko.rdfmapping.dsl.generator.rml.R2rmlDialect;
@@ -52,6 +54,12 @@ public class RdfMappingRuntimeModule extends AbstractRdfMappingRuntimeModule {
 
 	public Class<? extends IDefaultResourceDescriptionStrategy> bindIDefaultResourceDescriptionStrategy() {
 		return RdfMappingResourceDescriptionStrategy.class;
+	}
+
+	@Override
+	public Class<? extends IFormatter2> bindIFormatter2() {
+		// TODO return RdfMappingFormatter.class; (means delete this method completely)
+		return RealRdfMappingFormatter.class;
 	}
 
 }
