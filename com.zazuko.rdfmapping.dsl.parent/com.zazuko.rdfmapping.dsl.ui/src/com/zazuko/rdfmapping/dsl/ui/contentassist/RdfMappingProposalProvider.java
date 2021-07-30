b6f395326ea03db50bcdc82bafc891353b94a328
Christoph Gutmann <christoph.gutmann@gmx.ch>
2021-07-30 17:56:07 +0200

    remove styled string calculation #110
    this was done for the feature: Syntax involving prefixes with ':'
    instead of '.' #12 - so there is no replacement for this code.

--- com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.dsl.ui/src/com/zazuko/rdfmapping/dsl/ui/contentassist/RdfMappingProposalProvider.java
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.dsl.ui/src/com/zazuko/rdfmapping/dsl/ui/contentassist/RdfMappingProposalProvider.java
@@ -3,44 +3,11 @@
  */
 package com.zazuko.rdfmapping.dsl.ui.contentassist;
 
-import javax.inject.Inject;
-
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.jface.viewers.StyledString;
-import org.eclipse.xtext.naming.IQualifiedNameConverter;
-import org.eclipse.xtext.resource.IEObjectDescription;
-
-import com.zazuko.rdfmapping.dsl.rdfMapping.RdfClass;
-import com.zazuko.rdfmapping.dsl.rdfMapping.RdfProperty;
-import com.zazuko.rdfmapping.dsl.services.RdfPrefixedNameConverter;
-
 /**
  * See
  * https://www.eclipse.org/Xtext/documentation/310_eclipse_support.html#content-assist
  * on how to customize the content assistant.
  */
-@Deprecated
 public class RdfMappingProposalProvider extends AbstractRdfMappingProposalProvider {
-	@Inject
-	private IQualifiedNameConverter qualifiedNameConverter;
-
-	@Inject
-	private RdfPrefixedNameConverter rdfDslConverter;
-
-	
-
-	// TODO how to re-implement this?
-	protected StyledString getStyledDisplayString(IEObjectDescription description) {
-		EObject eo = description.getEObjectOrProxy();
-		if (eo instanceof RdfProperty //
-				|| eo instanceof RdfClass //
-		) {
-			String origQualifiedNameString = this.qualifiedNameConverter.toString(description.getQualifiedName());
-			String qualifiedNameString = this.rdfDslConverter.toString(origQualifiedNameString);
-			return getStyledDisplayString(eo, qualifiedNameString,
-					qualifiedNameConverter.toString(description.getName()));
-		}
-		return super.getStyledDisplayString(description);
-	}
 
 }
