72fa0732921c62c179edfe7c0004a7ab5bd3cece
Michael Rauch <git.mra@miranet.ch>
2021-10-20 17:06:39 +0200

    #110 bind RdfMappingIdeProposalProvider

--- com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.dsl.ide/src/com/zazuko/rdfmapping/dsl/ide/RdfMappingIdeModule.java
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.dsl.ide/src/com/zazuko/rdfmapping/dsl/ide/RdfMappingIdeModule.java
@@ -1,8 +1,23 @@
 package com.zazuko.rdfmapping.dsl.ide;
 
+import org.eclipse.xtext.ide.editor.contentassist.IdeContentProposalProvider;
+
+import com.zazuko.rdfmapping.dsl.ide.contentassist.RdfMappingIdeProposalProvider;
+
 /**
  * Use this class to register ide components.
  */
 public class RdfMappingIdeModule extends AbstractRdfMappingIdeModule {
 
+	public Class<? extends IdeContentProposalProvider> bindIdeContentProposalProvider() {
+		debug("binding RdfMappingIdeProposalProvider");
+		return RdfMappingIdeProposalProvider.class;
+	}
+
+	// in VS-code, this is visible in the tab "OUTPUT" for Xtext Server
+	@Deprecated
+	// TODO remove this
+	public static void debug(String message) {
+		System.err.println("############### " + message);
+	}
 }
