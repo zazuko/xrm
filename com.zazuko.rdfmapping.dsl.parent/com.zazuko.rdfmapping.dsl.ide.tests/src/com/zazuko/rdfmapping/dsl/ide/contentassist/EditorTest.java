ec680ad366ceb8f55ee5b6b455f58b74da28a59c
Christoph Gutmann <christoph.gutmann@gmx.ch>
2021-10-23 16:27:23 +0200

    #108 editortest completion tests

--- com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.dsl.ide.tests/src/com/zazuko/rdfmapping/dsl/ide/contentassist/EditorTest.java
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.dsl.ide.tests/src/com/zazuko/rdfmapping/dsl/ide/contentassist/EditorTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import com.zazuko.rdfmapping.dsl.ide.contentassist.util.MarkerContext;
 import com.zazuko.rdfmapping.dsl.ide.contentassist.util.PositionContext;
+import com.zazuko.rdfmapping.dsl.rdfMapping.RdfProperty;
 import com.zazuko.rdfmapping.dsl.rdfMapping.TemplateDeclaration;
 
 public class EditorTest extends EditorTestProjectDrivenTest {
@@ -25,6 +26,57 @@ public class EditorTest extends EditorTestProjectDrivenTest {
 		b.ref("exampleIri", TemplateDeclaration.class, position);
 		b.ref("UniqTemplate", TemplateDeclaration.class, position);
 		b.string("templateValue", position);
+		
+		testCompletion((TestCompletionConfiguration it) -> {
+			position.configure(it);
+			it.setExpectedCompletionItems(b.toExpectedCompletionItems());
+		});
+	}
+
+	@Test
+	public void refToRdfPropertThingSizeOfferedTest() {
+		MarkerContext ctx = this.marker("refToRdfPropertThingSizeOffered");
+		PositionContext position = ctx.first();
+		
+		CompletionExpectationBuilder b = new CompletionExpectationBuilder();
+		
+		this.assertionMode(AssertEqualsMode.EXPECTED_FOUND_IN_ACTUAL);
+		
+		b.ref("thing.size", RdfProperty.class, position);
+		
+		testCompletion((TestCompletionConfiguration it) -> {
+			position.configure(it);
+			it.setExpectedCompletionItems(b.toExpectedCompletionItems());
+		});
+	}
+
+	@Test
+	public void refToRdfPropertThingSizeOffered2Test() {
+		MarkerContext ctx = this.marker("refToRdfPropertThingSizeOffered2");
+		PositionContext position = ctx.first();
+		
+		CompletionExpectationBuilder b = new CompletionExpectationBuilder();
+		
+		this.assertionMode(AssertEqualsMode.EXPECTED_FOUND_IN_ACTUAL);
+		
+		b.ref("thing.size", RdfProperty.class, position);
+		
+		testCompletion((TestCompletionConfiguration it) -> {
+			position.configure(it);
+			it.setExpectedCompletionItems(b.toExpectedCompletionItems());
+		});
+	}
+
+	@Test
+	public void noProposalForMultiRefTest() {
+		MarkerContext ctx = this.marker("noProposalForMultiRef");
+		PositionContext position = ctx.nextLineWithTextAtStart("constant");
+		
+		CompletionExpectationBuilder b = new CompletionExpectationBuilder();
+		
+		this.assertionMode(AssertEqualsMode.EXPECTED_ABSENT_IN_ACTUAL);
+		
+		b.keyword("multi-reference", position);
 		
 		testCompletion((TestCompletionConfiguration it) -> {
 			position.configure(it);
--- com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.dsl.ide.tests/src/com/zazuko/rdfmapping/dsl/ide/contentassist/EditorTestProjectDrivenTest.java
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.dsl.ide.tests/src/com/zazuko/rdfmapping/dsl/ide/contentassist/EditorTestProjectDrivenTest.java
@@ -1,5 +1,7 @@
 package com.zazuko.rdfmapping.dsl.ide.contentassist;
 
+import org.junit.jupiter.api.Assertions;
+
 import com.zazuko.rdfmapping.dsl.ide.contentassist.util.FileContext;
 import com.zazuko.rdfmapping.dsl.ide.contentassist.util.MarkerContext;
 
@@ -8,6 +10,8 @@ public abstract class EditorTestProjectDrivenTest extends AbstractServerTest {
 	private static final String PATH = "../../runtime-EclipseXtext/editor-test";
 
 	private final FileContext fileContext;
+
+	private AssertEqualsMode assertEqualsMode = AssertEqualsMode.IDENTITCAL;
 
 	public EditorTestProjectDrivenTest(String fileName) {
 		this.fileContext = new FileContext(PATH, fileName);
@@ -19,6 +23,46 @@ public abstract class EditorTestProjectDrivenTest extends AbstractServerTest {
 
 	public MarkerContext marker(String marker) {
 		return this.fileContext.getMarker(marker);
+	}
+
+	public void assertionMode(AssertEqualsMode mode) {
+		this.assertEqualsMode = mode;
+	}
+
+	@Override
+	public void assertEquals(String expected, String actual) {
+		switch (this.assertEqualsMode) {
+		case IDENTITCAL:
+			super.assertEquals(expected, actual);
+			break;
+		case EXPECTED_FOUND_IN_ACTUAL:
+			assertExpectedInActual(expected, actual);
+			break;
+		case EXPECTED_ABSENT_IN_ACTUAL:
+			assertMissingInActual(expected, actual);
+			break;
+		default:
+			throw new RuntimeException(this.assertEqualsMode + "?");
+		}
+	}
+
+	private void assertExpectedInActual(String expected, String actual) {
+		Assertions.assertNotNull(actual);
+		Assertions.assertTrue(actual.contains(expected),
+				() -> String.format("expected to find '%s' in '%s'", expected, actual));
+	}
+
+	private void assertMissingInActual(String expected, String actual) {
+		Assertions.assertNotNull(actual);
+		Assertions.assertFalse(actual.contains(expected),
+				() -> String.format("expected NOT to find '%s' in '%s'", expected, actual));
+	}
+
+	static enum AssertEqualsMode {
+		IDENTITCAL, //
+		EXPECTED_FOUND_IN_ACTUAL, //
+		EXPECTED_ABSENT_IN_ACTUAL, //
+		;
 	}
 
 }
