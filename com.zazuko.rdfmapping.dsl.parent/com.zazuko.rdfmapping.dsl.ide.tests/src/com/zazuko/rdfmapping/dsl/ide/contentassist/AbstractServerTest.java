089556a41c555481b0b41468e5ea5f319f399d69
Christoph Gutmann <christoph.gutmann@gmx.ch>
2021-10-23 13:39:55 +0200

    #108 unit test launchable

---
+++ com.zazuko.rdfmapping.dsl.parent/com.zazuko.rdfmapping.dsl.ide.tests/src/com/zazuko/rdfmapping/dsl/ide/contentassist/AbstractServerTest.java
@@ -0,0 +1,10 @@
+package com.zazuko.rdfmapping.dsl.ide.contentassist;
+
+import org.eclipse.xtext.testing.AbstractLanguageServerTest;
+
+public abstract class AbstractServerTest extends AbstractLanguageServerTest {
+
+	public AbstractServerTest() {
+		super("xrm");
+	}
+}
